bf561aaf35748020f1388ca1e64363b9f4c1994b
Pavel Kolokdin <pk@napoleonit.ru>
2018-09-10 17:53:33 +0500

    ✨ add max phrase size in config

--- sick_bastard_api/views.py
+++ sick_bastard_api/views.py
@@ -1,4 +1,4 @@
 
 async def phrase(request, phrase):
-    answer = request.app.model.make_sentence()
+    answer = request.app.model.make_short_sentence(request.app.config.max_phrase_size)
     return {'answer': answer}
